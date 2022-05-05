c1ab7787b9b30e4c614f48b81d2fcad8fe4e5473
Annie <devannie777@gmail.com>
2022-05-05 11:36:36 +0100

    Props and styles for Contact component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,28 @@ import Contact from './components/Contact';
 
 function App() {
   return (<div>
-    <h1>My Contacts</h1>
-    <Contact />
+    <h1 className="heading">My Contacts</h1>
+    <Contact
+      name="Beyonce"
+      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      alt="avatar_img"
+      tel="+123 456 789"
+      email="b@beyonce.com"
+    />
+    <Contact
+      name="Madonna"
+      img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTds5xMDlZn6SR2ufT19rsZvkHpVGGvt8DcZQ&usqp=CAU"
+      alt="avatar_img"
+      tel="+123 44546 689"
+      email="m@madonna.com"
+    />
+    <Contact
+      name="Jack Bauer"
+      img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+      alt="avatar_img"
+      tel="+153 47846 569"
+      email="jack@nowhere.com"
+    />
 
   </div>
   );
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,18 @@
 import React from 'react'
 
-function Contact() {
+function Contact(props) {
   return (
-    <div>
-      <h2>Beyonce</h2>
-      <img
-        src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-        alt="avatar_img"
-      />
-      <p>+123 456 789</p>
-      <p>b@beyonce.com</p>
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <img className="circle-img"
+          src={props.img}
+        />
+      </div>
+      <div className="bottom">
+        <p className="info">{props.tel}</p>
+        <p className="info">{props.email}</p>
+      </div>
     </div>
   )
 }
